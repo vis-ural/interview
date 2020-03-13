496f3acefe8a3f1028b617a5a562e9c8273cb673
iLogachev <ilogachev@infomarketstudio.ru>
2020-03-14 01:48:57 +0300

    end

--- src/Pawn.php
+++ src/Pawn.php
@@ -9,17 +9,17 @@ class Pawn extends Figure
             $cur_to = $figures[$xTo][$yTo]->isBlack();
             if(($cur_from == $cur_to && $figures[$xTo][$yTo]!=$figures[$xFrom--][$yFrom++])
                 || $cur_from == $cur_to && $figures[$xTo][$yTo]!=$figures[$xFrom++][$yFrom++]){
-                throw new \Exception("Game rules violated ");
+                throw new \Exception("Game rules violated no kill myself");
             }
         } else {
             if($xFrom != $xTo && $yFrom !=$yTo){
-                throw new \Exception("Game rules violated ");
+                throw new \Exception("Game rules violated no step diagonals");
             }
         }
         $this->isBlack() ? --$yFrom : ++$yFrom;
         if (($yFrom == 3 && $yTo == 4) || ($yFrom == 6 && $yTo == 5) ) {
            if(isset($figures[$xFrom][$yFrom++])){
-                throw new \Exception("Game rules violated ");
+                throw new \Exception("Game rules violated no jumping");
             }
             $out = true;
         } else {
